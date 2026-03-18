0b67c7fa551554cb43e8a4131fb26a0c9fea71f8
Mahboob Hasan <hasanmahboob059@gmail.com>
2026-03-19 01:42:25 +0530

    feat: inbuilt commands

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
-
 
 int main(int agrc, char *agrv[]) {
 
@@ -12,10 +12,33 @@ int main(int agrc, char *agrv[]) {
 
     command[strcspn(command, "\n")] = 0;
 
-    if (strcmp(command, "exit") == 0) {
+    char first[5];
+    char args[100];
+    const char *inbuilt_command[] = {"echo", "type", "exit"};
+    int size = sizeof(inbuilt_command) / sizeof(inbuilt_command[0]);
+
+    sscanf(command, "%s %[^\n]", first, args);
+
+    if (strcmp(first, "exit") == 0) {
       break;
-    } else if (strncmp(command, "echo ", 5) == 0) {
-      printf("%s\n", command + 5);
+    } else if (strcmp(first, "echo") == 0) {
+      printf("%s\n", args);
+    } else if (strcmp(first, "type") == 0) {
+
+      bool flag = false;
+      for (int i = 0; i < size; i++) {
+        if (strcmp(args, inbuilt_command[i]) == 0) {
+          flag = true;
+          break;
+        }
+      }
+
+      if (flag) {
+        printf("%s is shell builtin\n", args);
+      } else {
+        printf("%s: not found\n", args);
+      }
+
     } else {
       printf("%s: command not found\n", command);
     }
