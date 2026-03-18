6b0c0c82e6418ebe2297fa6f3f56334ab7c1ed13
Mahboob Hasan <hasanmahboob059@gmail.com>
2026-03-19 00:47:49 +0530

    feat: echo

--- main.c
+++ main.c
@@ -14,9 +14,11 @@ int main(int agrc, char *agrv[]) {
 
     if (strcmp(command, "exit") == 0) {
       break;
+    } else if (strncmp(command, "echo ", 5) == 0) {
+      printf("%s\n", command + 5);
+    } else {
+      printf("%s: command not found\n", command);
     }
-
-    printf("%s: command not found\n", command);
   }
 
   return 0;
