93a602682c5c797ba8c735537d5476baad1b06e6
Mahboob Hasan <hasanmahboob059@gmail.com>
2026-03-19 00:17:10 +0530

    feat: use of user input ans strcspn

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main(int agrc, char *agrv[]) {
+  printf("$\n");
+
+  char command[1024];
+  fgets(command, sizeof(command), stdin);
+
+  command[strcspn(command, "\n")] = 0;
+
+  printf("%s: command not found\n", command);
+
+  return 0;
+}
