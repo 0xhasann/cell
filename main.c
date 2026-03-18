15b16e7f272429199482fe0da12d573c195fcc7b
Mahboob Hasan <hasanmahboob059@gmail.com>
2026-03-19 00:35:15 +0530

    feat: exit

--- main.c
+++ main.c
@@ -5,12 +5,16 @@
 int main(int agrc, char *agrv[]) {
 
   while (1) {
-    printf("$\n");
+    printf("$ ");
 
     char command[1024];
     fgets(command, sizeof(command), stdin);
 
     command[strcspn(command, "\n")] = 0;
+
+    if (strcmp(command, "exit") == 0) {
+      break;
+    }
 
     printf("%s: command not found\n", command);
   }
