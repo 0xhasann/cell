4b26cfce9161c9766c64edc02deaed995827f2ef
Mahboob Hasan <hasanmahboob059@gmail.com>
2026-03-30 23:25:01 +0530

    feat:listing n  history line

--- src/history.c
+++ src/history.c
@@ -1,10 +1,15 @@
 #include "path.h"
 #include <readline/history.h>
+#include <stdlib.h>
 
 void custom_history(char *args[]) {
-  (void)args;
 
-  for (int i = 0; i < history_length; i++) {
+  int his_start_index = 0;
+  if (args[1] != NULL) {
+    his_start_index = history_length - atoi(args[1]);
+  }
+
+  for (int i = his_start_index; i < history_length; i++) {
     HIST_ENTRY *entry = history_get(history_base + i);
     if (entry != NULL && entry->line != NULL) {
       printf("%d %s\n", history_base + i, entry->line);
